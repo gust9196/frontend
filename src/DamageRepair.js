27b1fa3a9411a52cade1dffb5c1f81308de74b2b
madsen10 <91535029+madsen10@users.noreply.github.com>
2023-12-04 22:25:08 +0100

    oprettelse af ny skade registrering virker

--- src/DamageRepair.js
+++ src/DamageRepair.js
@@ -1,92 +1,101 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import"./damagerepair.css"
+import "./damagerepair.css"
 
-
-function DamageRepair() {
-  const [formData, setFormData] = useState({
-    car: '',
-    leaseAgreement: '',
-    description: '',
-    registrationDate: '',
-    repairCost: '',
-  });
-
-  const [car, setCars] = useState([]);
-  const [leaseAgreement, setLeaseAgreements] = useState([]);
+const CreateDamage = () => {
+  const [cars, setCars] = useState([]);
+  const [leases, setLeases] = useState([]);
+  const [selectedCar, setSelectedCar] = useState('');
+  const [selectedLease, setSelectedLease] = useState('');
+  const [description, setDescription] = useState('');
+  const [registrationDate, setRegistrationDate] = useState('');
+  const [repairCost, setRepairCost] = useState('');
 
   useEffect(() => {
-    // Hent biler fra backend og opdater cars state
+    // Hent biler fra databasen ved komponentens indlæsning
     axios.get('http://localhost:4000/car')
       .then(response => setCars(response.data))
       .catch(error => console.error('Fejl ved hentning af biler:', error));
 
-    // Hent leaseaftaler fra backend og opdater leaseAgreements state
+    // Hent leaseaftaler fra databasen ved komponentens indlæsning
     axios.get('http://localhost:4000/leaseAgreement')
-      .then(response => setLeaseAgreements(response.data))
+      .then(response => setLeases(response.data))
       .catch(error => console.error('Fejl ved hentning af leaseaftaler:', error));
-  }, []);
+  }, []); // Tomt array betyder, at useEffect kun køres ved komponentens indlæsning
 
-  const handleChange = (e) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-
-  const handleSubmit = async (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
 
-    try {
-      const response = await axios.post('http://localhost:4000/damages/create', formData);
-      console.log('Skade oprettet:', response.data);
-      // Tilføj yderligere håndtering efter behov
-    } catch (error) {
-      console.error('Fejl ved oprettelse af skade:', error);
-      // Tilføj fejlhåndtering efter behov
-    }
+    // Opret skadeobjekt
+    const newDamage = {
+      car: {
+        carId: selectedCar
+      },
+      leaseAgreement: {
+        leaseId: selectedLease
+      },
+      description,
+      registrationDate,
+      repairCost: parseFloat(repairCost)
+    };
+
+    // Send POST-anmodning til backend
+    axios.post('http://localhost:4000/damages/create', newDamage)
+      .then(response => {
+        console.log('Skade oprettet:', response.data);
+        // Nulstil formular efter oprettelse
+        setSelectedCar('');
+        setSelectedLease('');
+        setDescription('');
+        setRegistrationDate('');
+        setRepairCost('');
+      })
+      .catch(error => console.error('Fejl ved oprettelse af skade:', error));
   };
 
   return (
     <div>
-      <h2>Opret Skaderegistrering</h2>
+      <h2>Opret Skade</h2>
       <form onSubmit={handleSubmit}>
         <label>
-          Bil:
-          <select name="car" value={formData.car} onChange={handleChange}>
-            <option value="" disabled>Vælg bil</option>
-            {car.map(car => (
-              <option key={car.carId} value={car.carId}>{car.carBrand} {car.model}</option>
+          Vælg bil:
+          <select value={selectedCar} onChange={(e) => setSelectedCar(e.target.value)}>
+            <option value="">Vælg en bil</option>
+            {cars.map(car => (
+              <option key={car.carId} value={car.carId}>{car.carBrand} - {car.model}</option>
             ))}
           </select>
         </label>
-
+        <br />
         <label>
-          Leaseaftale:
-          <select name="leaseAgreement" value={formData.leaseAgreement} onChange={handleChange}>
-    <option value="" disabled>Vælg leaseaftale</option>
-    {leaseAgreement.map(lease => (
-      <option key={lease.id} value={lease.id}>{lease.lease_id}</option>
-    ))}
+          Vælg leaseaftale:
+          <select value={selectedLease} onChange={(e) => setSelectedLease(e.target.value)}>
+            <option value="">Vælg en leaseaftale</option>
+            {leases.map(lease => (
+              <option key={lease.leaseId} value={lease.leaseId}>{lease.startDate} - {lease.endDate}</option>
+            ))}
           </select>
         </label>
-
+        <br />
         <label>
           Beskrivelse:
-          <textarea name="description" value={formData.description} onChange={handleChange} />
+          <textarea value={description} onChange={(e) => setDescription(e.target.value)} />
         </label>
-
+        <br />
         <label>
           Registreringsdato:
-          <input type="text" name="registrationDate" value={formData.registrationDate} onChange={handleChange} />
+          <input type="text" value={registrationDate} onChange={(e) => setRegistrationDate(e.target.value)} />
         </label>
-
+        <br />
         <label>
           Reparationsomkostninger:
-          <input type="text" name="repairCost" value={formData.repairCost} onChange={handleChange} />
+          <input type="text" value={repairCost} onChange={(e) => setRepairCost(e.target.value)} />
         </label>
-
+        <br />
         <button type="submit">Opret Skade</button>
       </form>
     </div>
   );
-}
+};
 
-export default DamageRepair;
+export default CreateDamage;

c545bba4dbbb250887dd039ea9539383da4374a7
arnefogh <andreasfogh7@gmail.com>
2023-12-05 11:44:13 +0100

    push

--- src/CustomersList.js
+++ src/CustomersList.js
@@ -23,9 +23,9 @@ const CustomerList = () => {
       <h2>Liste over kunder</h2>
       <div className="customer-grid">
         {customers.map(customer => (
-          <div key={customer.customerId} className="customer-item">
+          <div key={customer.customerID} className="customer-item">
             <div className="customer-details">
-              <p><strong>ID:</strong> {customer.customerId}</p>
+              <p><strong>ID:</strong> {customer.customerID}</p>
               <p><strong>Navn:</strong> {customer.name}</p>
               <p><strong>Adresse:</strong> {customer.address}</p>
               <p><strong>Telefon nummer:</strong> {customer.phoneNumber}</p>

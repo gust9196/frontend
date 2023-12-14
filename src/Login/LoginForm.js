cf524746d4147175986c137aabe04dbc589b8394
arnefogh <andreasfogh7@gmail.com>
2023-12-14 13:58:35 +0100

    s

--- src/Login/LoginForm.js
+++ src/Login/LoginForm.js
@@ -1,9 +1,13 @@
 import React, { useState } from "react";
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 import "./loginstyle.css";
 
 function LoginForm(props) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  let navigate = useNavigate();
 
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
@@ -15,26 +19,30 @@ function LoginForm(props) {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    if (!email || !password) {
+      setError("Udfyld venligst alle felter");
+      return;
+    }
+  
     try {
-      const response = await fetch("/api/login", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ email, password }),
-      });
-      const data = await response.json();
-      if (data.success) {
+      const response = await axios.get(`http://localhost:4000/user?email=${encodeURIComponent(email)}&password=${encodeURIComponent(password)}`);
+      if (response.data.valid) {
+        navigate("/");
+        setError("");
       } else {
+        setError("Email eller kodeord er forkert");
       }
-    } catch (error) {}
+    } catch (error) {
+      console.error("Login fejl", error);
+      setError("Der opstod en fejl ved login");
+    }
   };
 
   return (
     <>
       <div className="main-container">
         <div className="image-wrapper">
-          <img
+        <img
             loading="lazy"
             srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/bd920615-a09b-45ac-8454-71e12a613401?apiKey=5aea209633a743968e186ed24c6841a0&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/bd920615-a09b-45ac-8454-71e12a613401?apiKey=5aea209633a743968e186ed24c6841a0&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/bd920615-a09b-45ac-8454-71e12a613401?apiKey=5aea209633a743968e186ed24c6841a0&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/bd920615-a09b-45ac-8454-71e12a613401?apiKey=5aea209633a743968e186ed24c6841a0&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/bd920615-a09b-45ac-8454-71e12a613401?apiKey=5aea209633a743968e186ed24c6841a0&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/bd920615-a09b-45ac-8454-71e12a613401?apiKey=5aea209633a743968e186ed24c6841a0&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/bd920615-a09b-45ac-8454-71e12a613401?apiKey=5aea209633a743968e186ed24c6841a0&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/bd920615-a09b-45ac-8454-71e12a613401?apiKey=5aea209633a743968e186ed24c6841a0&"
             className="image"
@@ -55,6 +63,7 @@ function LoginForm(props) {
             value={password}
             onChange={handlePasswordChange}
           />
+          {error && <div className="error-message">{error}</div>}
           <button className="login-button" onClick={handleSubmit}>
             Log ind
           </button>
